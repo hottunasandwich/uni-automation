a81ac92b6ffb258bd927252dadff2ecb97e156ba
Rodrik Vartoomian <55018126+hottunasandwich@users.noreply.github.com>
2022-01-24 01:32:00 +0330

    Update robot.py

--- robot.py
+++ robot.py
@@ -4,4 +4,3 @@ password = '_password_'
 units = ['350170201', '350170201', '350170201', '350170201']
 robot = Golestan(username,password,units)
 robot.login().goToPishkhaan().goToMainEntekhaabVahed().fillT01rows()
-print('fuck golestan')
